124dfeb1464446f6b65d11b4fb881ff67401178c
Kavishu Goyal <kavishu28goyal@gmail.com>
2024-10-08 20:12:01 +0530

    median of array

--- demo/src/main/java/com/example/demo/ThreadExamples.java
+++ demo/src/main/java/com/example/demo/ThreadExamples.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.Arrays;
 import java.util.concurrent.Semaphore;
 
 public class ThreadExamples {
@@ -21,9 +22,36 @@ public class ThreadExamples {
 			}).start();
 		}
 	}
-	
+
 	String test() {
 		return "Testing";
+	}
+
+}
+
+class Median {
+
+	public static void main(String[] args) {
+		int[] a1 = { 1, 3, 5, 7 };
+		int[] a2 = { 2, 4, 6, 8 };
+		int[] combined = new int[a1.length + a2.length];
+		int k = 0;
+
+		for (int i : a1) {
+			combined[k++] = i;
+		}
+		for (int j : a2) {
+			combined[k++] = j;
+		}
+		java.util.Arrays.sort(combined);
+		Arrays.stream(combined).forEach(val -> System.out.print(val + " "));
+		int left = 0;
+		int right = combined.length - 1;
+		int mid = left + (right - left) / 2;
+		if (combined.length % 2 == 0)
+			System.out.println("Median: " + (combined[mid] + combined[mid + 1]) / 2.0); // for even length array
+		else
+			System.out.println("Median: " + combined[mid]); // for odd length array
 	}
 
 }
